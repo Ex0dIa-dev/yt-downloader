d423a80bd763d7ea8b0c461909426523806910e1
Ex0dIa-dev <lucafranze3012@gmail.com>
2020-08-19 15:47:41 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,61 @@
+### yt-downloader
+
+**Dependencies:**
+
+- [youtube-dl](https://github.com/ytdl-org/youtube-dl)
+- [ffmpeg](https://ffmpeg.org/)
+
+****
+
+**Build:**
+
+```go
+go build yt-downloader.go
+```
+
+
+
+**Use:**
+
+```bash
+./yt-downloader -u 'url'
+```
+
+```bash
+./yt-downloader -u 'url' -f mp3
+```
+
+```bash
+./yt-downloader -u 'url' -o song.mp3
+```
+
+
+
+**Example:**
+
+```bash
+./yt-downloader -u 'https://www.youtube.com/watch?v=pPw_izFr5PA'
+```
+
+```bash
+./yt-downloader -u 'https://www.youtube.com/watch?v=pPw_izFr5PA' -f mp4 
+```
+
+```bash
+./yt-downloader -u 'https://www.youtube.com/watch?v=pPw_izFr5PA' -o gooba.mp3
+```
+
+```bash
+./yt-downloader -u 'https://www.youtube.com/watch?v=pPw_izFr5PA' -f mp4 -o gooba.mp4
+```
+
+****
+
+
+
+**Flag:**
+
+- **'-u'** ---> define youtube video url
+- **'-f'** ---> define file format("mp3" or "mp4"; default: mp3)
+- **'-o'** ---> define output filename(ex. 'song.mp3' or 'video.mp4')
+
---
+++ yt-downloader.go
@@ -0,0 +1,254 @@
+/*
+	MADE BY Ex0dIa-dev
+*/
+
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+/*
+	1. DOWNLOAD AUDIO AND VIDEO FROM URL(two goroutines)
+	2. FFMPEG .WEBM to MP3
+	3. FFMPEG MERGE MP4 and MP
+*/
+
+func init() {
+	flag.StringVar(&url, "u", "", "insert url")
+	flag.StringVar(&format, "f", "mp3", "mp4 or mp3")
+	flag.StringVar(&output, "o", "", "output filename")
+}
+
+var url, format, output string
+
+func main() {
+
+	flag.Parse()
+
+	if url == "" {
+		fmt.Println("[-]Please insert url.")
+		os.Exit(1)
+	}
+
+	switch format {
+	case "mp4":
+		//getting direct url from youtube-dl
+		mp4_url, mp3_url := GetDownloadUrl(url)
+		tmp_audio_webm := "tmp_audio.webm"
+		tmp_video := "tmp_video.mp4"
+
+		//downloading audio and video
+		fmt.Println("[+]Downloading...")
+		go DownloadVideo(mp4_url, tmp_video)
+		DownloadAudio(mp3_url, tmp_audio_webm)
+		fmt.Println("[+]Download complete.")
+
+		//converting audio.webm to audio.mp3
+		fmt.Println("[+]Processing file...")
+		tmp_audio_mp3 := "tmp_audio.mp3"
+		WebmToMp3(tmp_audio_webm, tmp_audio_mp3)
+
+		//merging mp4 and mp3
+
+		if output == "" {
+			output = GetVideoTitle(url)
+			output = output + ".mp4"
+		}
+
+		MergeAudioVideo(output, tmp_video, tmp_audio_mp3)
+
+		fmt.Println("[+]Done.")
+	case "mp3":
+		_, mp3_url := GetDownloadUrl(url)
+		tmp_audio_webm := "tmp_audio.webm"
+
+		//downloading audio
+		fmt.Println("[+]Downloading...")
+		DownloadAudio(mp3_url, tmp_audio_webm)
+		fmt.Println("[+]Download complete.")
+
+		//converting audio.webm to audio.mp3
+		fmt.Println("[+]Processing file...")
+
+		if output == "" {
+			output = GetVideoTitle(url)
+			output = output + ".mp3"
+		}
+
+		WebmToMp3(tmp_audio_webm, output)
+
+		fmt.Println("[+]Done.")
+	}
+
+}
+
+//get the mp3 and mp4 downlaod url from youtube-dl
+func GetDownloadUrl(url string) (string, string) {
+
+	if FileExists("url.txt") {
+		err := os.Remove("url.txt")
+		checkerr(err)
+	}
+
+	file, err := os.Create("url.txt")
+	checkerr(err)
+
+	cmd := exec.Command("youtube-dl", "--get-url", url)
+	cmd.Stdout = file
+	err = cmd.Run()
+	checkerr(err)
+
+	file.Close()
+
+	file, err = os.Open("url.txt")
+	checkerr(err)
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Split(bufio.ScanLines)
+
+	var lines []string
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	mp4_url := lines[0]
+	mp3_url := lines[1]
+
+	err = os.Remove("url.txt")
+	checkerr(err)
+
+	return mp4_url, mp3_url
+}
+
+func GetVideoTitle(url string) string {
+
+	out, err := exec.Command("youtube-dl", "-e", url).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	title := string(out)
+	title = strings.Replace(title, "\n", "", -1)
+
+	return title
+}
+
+func DownloadVideo(url, filename string) {
+
+	if FileExists(filename) {
+		err := os.Remove(filename)
+		checkerr(err)
+	}
+
+	file, err := os.Create(filename)
+	checkerr(err)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		file.Close()
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if _, err = io.Copy(file, resp.Body); err != nil {
+		file.Close()
+		log.Fatal(err)
+	}
+
+	file.Close()
+}
+
+func DownloadAudio(url, filename string) {
+
+	if FileExists(filename) {
+		err := os.Remove(filename)
+		checkerr(err)
+	}
+
+	file, err := os.Create(filename)
+	checkerr(err)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		file.Close()
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if _, err = io.Copy(file, resp.Body); err != nil {
+		file.Close()
+		log.Fatal(err)
+	}
+
+	file.Close()
+
+}
+
+func WebmToMp3(in_filename, out_filename string) {
+
+	cmd := exec.Command("ffmpeg", "-i", in_filename, "-vn", "-ab", "128k", "-ar", "44100", "-y", out_filename)
+	err := cmd.Run()
+	checkerr(err)
+
+	err = os.Remove(in_filename)
+	checkerr(err)
+}
+
+func MergeAudioVideo(output_filename, mp4_path, mp3_path string) {
+
+	cmd := exec.Command("ffmpeg", "-i", mp4_path, "-i", mp3_path, "-map", "0:v", "-map", "1:a", "-c:v", "copy", "-c:a", "copy", "-y", output_filename)
+	err := cmd.Run()
+	checkerr(err)
+
+	err = os.Remove(mp4_path)
+	checkerr(err)
+	err = os.Remove(mp3_path)
+	checkerr(err)
+}
+
+/* PROGRESS STATUS
+
+type DownloadCounter struct {
+	Total uint64
+}
+
+func (dc *DownloadCounter) Write(p []byte) (int, error) {
+	n := len(p)
+	dc.Total += uint64(n)
+	dc.PrintProgress()
+	return n, nil
+}
+
+func (dc DownloadCounter) PrintProgress() {
+	fmt.Printf("\r%s", strings.Repeat(" ", 35))
+
+	fmt.Printf("\rDownloading... %s complete.", humanize.Bytes(dc.Total))
+}
+*/
+
+func FileExists(filename string) bool {
+
+	stat, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+
+	return !stat.IsDir()
+}
+
+func checkerr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
